aebc99eec8dc615983f3d46b1105257ccb6641ca
Admin <eizinhtay1116@gmail.com>
2023-07-10 00:05:15 +0630

    update keys code

--- app/src/main/java/com/example/MyInputMethodService.kt
+++ app/src/main/java/com/example/MyInputMethodService.kt
@@ -3,10 +3,11 @@ package com.example
 import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
-import android.os.Handler
 import android.text.TextUtils
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputConnection
 import com.example.customkeyboard.R
 
 
@@ -16,6 +17,8 @@ class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionLi
     private var keyboard: Keyboard? = null
 
     private var caps = false
+    private var shiftKeyboard = false
+
 
     override fun onCreateInputView(): View? {
         keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView?
@@ -23,14 +26,47 @@ class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionLi
         keyboardView!!.keyboard = keyboard
         keyboardView!!.setOnKeyboardActionListener(this)
         return keyboardView
-
-//        return layoutInflater.inflate(R.layout.keyboard_view, null).apply {
-//            if (this is MyKeyboardView) {
-//                setOnKeyboardActionListener(this@MyInputMethod)
-//                keyboard = latinKeyboard
-//            }
-//        }
     }
+    private fun switchKeyboard(inputConnection: InputConnection, i: Int) {
+        Log.d("DEBUG", "I: $i")
+
+        Log.d("middle", "switchKeyboard: before switch")
+        when (i) {
+            -11 -> {
+//                key_family = 4677
+//                first_time_selected = 0
+                keyboard = Keyboard(this, R.xml.mm_unicode)
+                keyboardView!!.keyboard = keyboard
+                keyboardView!!.setOnKeyboardActionListener(this)
+            }
+            -2 -> {
+//                key_family = 4677
+//                first_time_selected = 0
+                shiftKeyboard=true
+                keyboard = Keyboard(this, R.xml.mm_unicode_shift)
+                keyboardView!!.keyboard = keyboard
+                keyboardView!!.setOnKeyboardActionListener(this)
+            }
+            -1 -> {
+//                key_family = 4677
+//                first_time_selected = 0
+                shiftKeyboard=true
+                keyboard = Keyboard(this, R.xml.mm_unicode_shift)
+                keyboardView!!.keyboard = keyboard
+                keyboardView!!.setOnKeyboardActionListener(this)
+            }
+            else->{
+                keyboard = Keyboard(this, R.xml.mm_unicode)
+                keyboardView!!.keyboard = keyboard
+                keyboardView!!.setOnKeyboardActionListener(this)
+            }
+
+        }
+    }
+
+//    override fun switchInputMethod(id: String?) {
+//        super.switchInputMethod(id)
+//    }
 
     override fun onPress(primaryCode: Int) {
 
@@ -62,9 +98,12 @@ class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionLi
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
         val inputConnection = currentInputConnection
+        switchKeyboard(inputConnection, primaryCode)
+
         if (inputConnection != null) {
             when (primaryCode) {
                 Keyboard.KEYCODE_DELETE -> {
+
                     val selectedText = inputConnection.getSelectedText(0)
                     if (TextUtils.isEmpty(selectedText)) {
                         inputConnection.deleteSurroundingText(1, 0)
@@ -72,6 +111,7 @@ class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionLi
                         inputConnection.commitText("", 1)
                     }
                     caps = !caps
+                    shiftKeyboard =!shiftKeyboard
                     keyboard!!.isShifted = caps
                     keyboardView!!.invalidateAllKeys()
                 }
@@ -79,6 +119,7 @@ class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionLi
                 Keyboard.KEYCODE_SHIFT -> {
                     caps = !caps
                     keyboard!!.isShifted = caps
+                    shiftKeyboard =!shiftKeyboard
                     keyboardView!!.invalidateAllKeys()
                 }
 
